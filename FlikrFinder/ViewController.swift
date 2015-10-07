4c0eccd9d057fd1f3230f0871cff1b2db6f45242
Michael Johnston <lastobelus@mac.com>
2015-10-07 16:25:55 -0700

    Initial Commit

---
+++ FlikrFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlikrFinder
+//
+//  Created by Michael Johnston on 07.10.2015.
+//  Copyright © 2015 Metafeat Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
