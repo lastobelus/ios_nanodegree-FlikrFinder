1655ca80f5565c34b9476c397666a7142e30984f
Michael Johnston <lastobelus@mac.com>
2015-10-15 14:12:33 -0700

    initial layout of view

--- FlikrFinder/ViewController.swift
+++ FlikrFinder/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var image: UIImageView!
+  @IBOutlet weak var nameSearchTextField: UITextField!
+  @IBOutlet weak var latitudeSearchTextField: UITextField!
+  @IBOutlet weak var longitudeSearchTextField: UITextField!
+  @IBOutlet weak var imageTitle: UILabel!
+
+
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,10 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  @IBAction func performNameSeach(sender: UIButton) {
+  }
 
+  @IBAction func performGeoSearch(sender: UIButton) {
+  }
 }
 
